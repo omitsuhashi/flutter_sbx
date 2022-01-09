4ac9d83e56574bf327e161f760bfe6851aed2145
Osamu Mitsuhashi <osamu.mitsuhashi224@gmail.com>
2022-01-09 12:00:06 +0900

    add: immutable programming
    using riverpod

--- lib/apps/provider/home.dart
+++ lib/apps/provider/home.dart
@@ -3,12 +3,34 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final helloWorldProvider = Provider<String>((ref) => 'Hello World');
 
+class CountModel {
+  final int count;
+
+  CountModel({this.count = 0});
+
+  CountModel copyWith(int count) {
+    return CountModel(count: count);
+  }
+}
+
+class CounterState extends StateNotifier<CountModel> {
+  CounterState(CountModel state) : super(state);
+
+  countUp() {
+    state = state.copyWith(state.count + 1);
+  }
+}
+
+final counterProvider = StateNotifierProvider<CounterState, CountModel>(
+    (ref) => CounterState(CountModel()));
+
 class ProviderApp extends ConsumerWidget {
   const ProviderApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final String hello = ref.watch(helloWorldProvider);
+    final counter = ref.watch(counterProvider.select((value) => value.count));
 
     return Scaffold(
         appBar: AppBar(
@@ -17,7 +39,13 @@ class ProviderApp extends ConsumerWidget {
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: [Text(hello)],
+            children: [
+              Text(hello),
+              Text(counter.toString()),
+              ElevatedButton(
+                  onPressed: ref.read(counterProvider.notifier).countUp,
+                  child: const Text('count up'))
+            ],
           ),
         ));
   }
